bf4543a17f49e58fa9c75133fdc012354bee2d5e
Chad Yates <cayates1989@gmail.com>
2017-08-16 16:54:22 -0400

    first commit with just the basic form elements built out on the page

---
+++ server.js
@@ -0,0 +1,52 @@
+// const variables
+
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+const dal = require('./dal');
+const bodyParser = require('body-parser')
+
+// reassignable variables
+
+// let todos = [
+//     "Wash the car",
+//     "Clean my room",
+//     "Brush the dog",
+//     "Make dinner",
+//     "Go for a run",
+//     "Finish my homework",
+//     "Finish my Iron Yard Reading"
+// ];
+
+// setting up mustache basics
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+// set up public folder for css  
+
+app.use(express.static('public'));
+
+// set up bodyParser
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// routes
+
+app.get ("/", function(req, res){
+    res.render('addtodobar');
+})
+
+app.get("/", function(req, res){
+    res.render(todoitems);
+})
+
+// setting up port
+
+app.set('port', 3000)
+
+app.listen(app.get('port'), function () {
+  console.log('This application is up and running, bro.')
+})
